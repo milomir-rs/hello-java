29633e3f54fe96f2d5d69e43dba245fb1e90e0e5
Milomir Dragović <hello@milomir.rs>
2022-04-13 09:19:01 +0200

    first java code

---
+++ Main.java
@@ -0,0 +1,25 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java, this is Milomir");
+        int firstNumber = 10;
+        int secondNumber = 15;
+        int sum = firstNumber + secondNumber;
+        char letterA = 'A';
+
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        System.out.println(sum);
+
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Osoba je punoletna");
+        } else {
+            System.out.println("Osoba je maloletna");
+        }
+
+    }
+}
